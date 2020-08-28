69fcc1d5abc539ecb16974126ddf50aa2eeaba50
muhammadamran <amranhakim9@gmail.com>
2020-08-28 16:40:41 +0700

    Initial commit

--- web-rskg/application/views/include/footer.php
+++ web-rskg/application/views/include/footer.php
@@ -1,41 +1,165 @@
-<footer class="ftco-footer ftco-bg-dark ftco-section">
-	<div class="container px-md-5">
-		<div class="row mb-5">
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4 ml-md-4">
-					<h2 class="ftco-heading-2">Category</h2>
-					<ul class="list-unstyled categories">
-						<li><a href="#">Photography <span>(6)</span></a></li>
-						<li><a href="#">Fashion <span>(8)</span></a></li>
-						<li><a href="#">Technology <span>(2)</span></a></li>
-						<li><a href="#">Travel <span>(2)</span></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Archives</h2>
-					<ul class="list-unstyled categories">
-						<li><a href="#">October 2018 <span>(6)</span></a></li>
-						<li><a href="#">September 2018 <span>(6)</span></a></li>
-						<li><a href="#">August 2018 <span>(8)</span></a></li>
-						<li><a href="#">July 2018 <span>(2)</span></a></li>
-						<li><a href="#">June 2018 <span>(7)</span></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Have a Questions?</h2>
-					<div class="block-23 mb-3">
-						<ul>
-							<li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-							<li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-							<li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</footer>
+<!-- footer-28 block -->
+<section class="w3l-medpill-footer ">
+  <footer class="footer-28">
+    <div class="footer-bg-layer">
+      <div class="container py-lg-3">
+        <div class="row footer-top-28">
+          <div class="col-lg-4 col-md-6 col-sm-7 footer-list-28 mt-sm-5 mt-4">
+            <h6 class="footer-title-28">Contact information</h6>
+            <ul class="address">
+              <li>
+                <p> #135 block, Barnard St. Brooklyn, <br>NY 10036, USA</p>
+              </li>
+              <li class="mt-4">
+                <p><span class="fa fa-phone"></span> 
+                  <a href="tel:+404-11-22-89">+1-2345-345-678-11</a>
+                </p>
+              </li>
+              <li>
+                <p><span class="fa fa-envelope"></span> 
+                  <a href="mailto:medpillhospital@mail.com">medpillhospital@mail.com</a>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-5 footer-list-28 mt-sm-5 mt-4">
+            <h6 class="footer-title-28">Company</h6>
+            <ul>
+              <li><a href="#url">Mission and values</a></li>
+              <li><a href="#url">Publications and reports</a></li>
+              <li><a href="#url">Ladership and Awards</a></li>
+              <li><a href="#url">Diversity is Our Specialty</a></li>
+              <li><a href="#url">Policies & Procedures</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-2 col-md-6 col-sm-4 footer-list-28 mt-sm-5 mt-4">
+            <h6 class="footer-title-28">Our Services</h6>
+            <ul>
+              <li><a href="#url">Orthopaedic</a></li>
+              <li><a href="#url">Dental Service</a></li>
+              <li><a href="#url">Lung Diseases</a></li>
+              <li><a href="#url">Heart Attact</a></li>
+              <li><a href="#url">Sport Injury</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-8 footer-list-28 mt-sm-5 mt-4">
+            <h6 class="footer-title-28">Hospital hours</h6>
+            <ul class="timing mb-lg-4">
+              <li><a href="#URL"><span class="fa fa-clock-o"></span>Mon to Fri : <span>08:00 - 20:00</span></a></li>
+              <li><a href="#URL"><span class="fa fa-clock-o"></span>Saturday : <span>08:00 - 20:00</span></a></li>
+              <li><a href="#URL"><span class="fa fa-clock-o"></span>Sunday : <span>08:00 - 20:00</span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="midd-footer-28 align-center py-lg-4 py-3 mt-md-5 mt-3">
+      <div class="container">
+        <p class="copy-footer-28 text-center"> &copy; 2020 Medpill. All Rights Reserved. Design by 
+          <a href="https://w3layouts.com/">W3Layouts</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+  <button onclick="topFunction()" id="movetop" title="Go to top">
+    &#10548;
+  </button>
+  <script>
+    window.onscroll = function () {
+      scrollFunction()
+    };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+      } else {
+        document.getElementById("movetop").style.display = "none";
+      }
+    }
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  </script>
+  <script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js');?>"></script>
+  <script>
+    $('#notify').change(function () {
+      if ($('#notify').is("Active")) {
+        $('body').css('overflow', 'hidden');
+      } else {
+        $('body').css('overflow', 'auto');
+      }
+    });
+  </script>
+  <script src="<?php echo base_url('assets/js/owl.carousel.js');?>"></script>
+  <script>
+    $(document).ready(function () {
+      $('.owl-one').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsiveClass: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          480: {
+            items: 1,
+            nav: false
+          },
+          667: {
+            items: 1,
+            nav: true
+          },
+          1000: {
+            items: 1,
+            nav: true
+          }
+        }
+      })
+    })
+  </script>
+  <script>
+    $(function () {
+      $('.navbar-toggler').click(function () {
+        $('body').toggleClass('noscroll');
+      })
+    });
+  </script>
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+</section>
+
+<!-- GetButton.io widget -->
+<script type="text/javascript">
+  (function () {
+    var options = {
+      <?php
+      $con=mysqli_connect("localhost","root","","db_kepasaraja");
+      if (mysqli_connect_errno())
+      {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+      $result = mysqli_query($con,"SELECT * FROM tb_profile ORDER BY id DESC");
+
+      if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_array($result))
+        {
+          ?>
+          whatsapp: "<?php echo $row['telepone'];?>", // WhatsApp number
+        <?php } } mysqli_close($con); ?>
+          call_to_action: "Pesan disini", // Call to action
+          position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+      })();
+    </script>
+    <!-- /GetButton.io widget -->
--- web-rskg/application/views/index.php
+++ web-rskg/application/views/index.php
@@ -1,73 +1,358 @@
-
-<?php 
-if(isset($_GET['cari'])){
-	$cari = $_GET['cari'];
-}
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<?php require('include/head.php');?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Medpill a Medical Category Responsive Web Template - Home : W3Layouts</title>
+	<link href="https://fonts.googleapis.com/css?family=Muli:400,600,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style-starter.css');?>">
+</head>
 <body>
-	<div id="colorlib-page">
-		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-		<?php require('include/sidebar.php');?>
-		<div id="colorlib-main">
-			<section class="ftco-section">
-				<div class="container">
-					<div class="row justify-content-center mb-5 pb-2">
-						<div class="col-md-7 heading-section text-center ftco-animate">
-							<div align="center">
-								<img src="<?php echo base_url('assets/data/img/all-rs/rskgcare.png') ?>" width="300px">
-							</div>
+	<!--w3l-header-->
+	<section class="w3l-top-header-content">
+		<div class="hny-top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="top-left col-lg-6">
+						<ul class="accounts">
+							<li class="top_li"><span class="fa fa-map-o"></span> 
+								<a href="#">Jl. Tubagus Ismail Raya No.46. Kota Bandung 40134</a>
+							</li>
+							<li class="top_li mr-lg-0"><span class="fa fa-envelope-o"></span> 
+								<a href="mailto:mail@company.com" class="mail"> mail@company.com</a>
+							</li>
+						</ul>
+					</div>
+					<div class="social-top col-lg-6 mt-lg-0 mt-sm-3">
+						<div class="top-bar-text">
+							<a class="bk-button" href="#">REGISTRASI ONLINE </a> 
+							You can request appointment in 24 hours
 						</div>
 					</div>
-					<div align="center">
-						<form method="get" action="" class="search-form">
-							<div class="form-group">    
-								<input type="text" name="cari" class="form-control" placeholder="Cari nama aplikasi disini...">
-								<br>
-								<button class="btn btn-dark btn-block" type="submit"> Klik disini</button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--//top-header-content-->
+	<section class="w3l-bootstrap-header">
+		<nav class="navbar navbar-expand-md navbar-light py-3">
+			<div class="container">
+				<a class="navbar-brand" href="index.html">
+					<img src="<?php echo base_url('assets/images/logo.png');?>" class="img-fluid" width="300px">
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					Menu
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.html">Home</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang Kami</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="blog.html">Profil Rumah Sakit</a>
+								<a class="dropdown-item" href="blog.html">Struktur Organisasi</a>
+								<a class="dropdown-item" href="blog-single.html">Indikator Mutu</a>
 							</div>
-						</form>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Layanan & Fasilitas</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="blog.html">Instalasi Gawat Darurat (IGD)</a>
+								<a class="dropdown-item" href="blog-single.html">Hemodialisis</a>
+								<a class="dropdown-item" href="blog-single.html">Rawat Jalan</a>
+								<a class="dropdown-item" href="blog-single.html">Rawat Inap</a>
+								<a class="dropdown-item" href="blog-single.html">CAPD</a>
+								<a class="dropdown-item" href="blog-single.html">Pelayanan Bedah</a>
+								<a class="dropdown-item" href="blog-single.html">Radiologi</a>
+								<a class="dropdown-item" href="blog-single.html">Laboratorium</a>
+								<a class="dropdown-item" href="blog-single.html">Instalasi Farmasi</a>
+								<a class="dropdown-item" href="blog-single.html">Medical Check Up</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="services.html">Jadwal Dokter</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Informasi Publik</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="blog.html">Info BPJS</a>
+								<a class="dropdown-item" href="blog.html">Kegiatan Ilmiah</a>
+								<a class="dropdown-item" href="blog.html">Galeri</a>
+								<a class="dropdown-item" href="blog.html">Lowongan Kerja</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact.html">Kontak</a>
+						</li>
+					</ul>
+				</div>
+				<a href="#domain" class="domain ml-3" data-toggle="modal" data-target="#DomainModal">
+					<div class="hamburger1">
+						<div></div>
+						<div></div>
+						<div></div>
 					</div>
-					<br>
-					<hr>
-					<div class="row">
-						<?php
-						$con=mysqli_connect("localhost","root","","rskg_care");
-						if (mysqli_connect_errno())
-						{
-							echo "Failed to connect to MySQL: " . mysqli_connect_error();
-						}
-						if(isset($_GET['cari'])){
-							$cari = $_GET['cari'];
-							$data = mysqli_query($con,"SELECT * FROM tb_aplikasi WHERE nama_app LIKE '%".$cari."%'");				
-						}else{
-							$data = mysqli_query($con,"SELECT * FROM tb_aplikasi");		
-						}
-						$no = 1;
-						while($row = mysqli_fetch_array($data)){
-						?>
-							<div class="col-md-4">
-								<div class="blog-entry ftco-animate">
-									<a href="#" class="img img-2" style="background-image: url(<?php echo base_url('assets/images/bgcover/'.$row['cover'])?>);"></a>
-									<div class="text text-2 pt-2 mt-3">
-										<span class="category mb-3 d-block"><a href="#"><?php echo $row['status_akses'] ?></a></span>
-										<h3 class="mb-4"><a href="#"><?php echo $row['nama_app'] ?></a></h3>
-										<a href="<?php echo $row['url']?>" target="_blank">
-											<button class="btn btn-dark btn-block">
-												<font style="color: #fff">Buka Aplikasi</font>
-											</button>
-										</a>
+				</a>
+			</div>
+		</nav>
+	</section>
+	<div class="modal right fade" id="DomainModal" tabindex="-1" role="dialog" aria-labelledby="DomainModalLabel2">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<div class="modal-body">
+					<div class="modal__content">
+						<h2 class="logo"><img src="<?php echo base_url('assets/images/logo.png');?>" class="img-fluid" width="300px"></h2>
+						<p class="mt-md-3 mt-2">Lorem ipsum dolor sit amet, elit. Eos expedita ipsam at fugiat ab.</p>
+						<div class="widget-menu-items mt-sm-5 mt-4">
+							<h5 class="widget-title">Menu Items</h5>
+							<nav class="navbar p-0">
+								<ul class="navbar-nav">
+									<li class="nav-item">
+										<a class="nav-link" href="index.html">Home</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="about.html">About</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="services.html">Services</a>
+									</li>
+									<li class="nav-item dropdown active">
+										<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+										<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+											<a class="dropdown-item" href="blog.html">Blog</a>
+											<a class="dropdown-item" href="blog-single.html">Blog Single</a>
+										</div>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="contact.html">Contact</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+						<div class="widget-social-icons mt-sm-5 mt-4">
+							<h5 class="widget-title">Contact Us</h5>
+							<ul class="icon-rounded address">
+								<li>
+									<p> #135 block, Barnard St. Brooklyn, <br>NY 10036, USA</p>
+								</li>
+								<li class="mt-3">
+									<p>
+										<span class="fa fa-phone"></span> 
+										<a href="tel:+404-11-22-89">+1-2345-345-678-11</a>
+									</p>
+								</li>
+								<li class="mt-2">
+									<p>
+										<span class="fa fa-envelope"></span> 
+										<a href="mailto:medpillhospital@mail.com">medpillhospital@mail.com</a>
+									</p>
+								</li>
+								<li class="top_li1 mt-2">
+									<p>
+										<span class="fa fa-clock-o"></span> 
+										<a href="mailto:medpillhospital@mail.com">Mon - Sun 09:00 - 21:00</a>
+									</p>
+								</li>
+							</ul>
+						</div>
+						<div class="widget-social-icons mt-sm-5 mt-4">
+							<h5 class="widget-title">Follow Us</h5>
+							<ul class="icon-rounded">
+								<li><a class="social-link twitter" href="#url" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li><a class="social-link linkedin" href="#url" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+								<li><a class="social-link tumblr" href="#url" target="_blank"><i class="fa fa-tumblr"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section class="w3l-main-slider" id="home">
+		<div class="companies20-content">
+			<div class="owl-one owl-carousel owl-theme">
+				<div class="item">
+					<li>
+						<div class="slider-info banner-view bg bg2">
+							<div class="banner-info">
+								<div class="container">
+									<div class="banner-info-bg">
+										<h5>We Provide total Health care solution</h5>
+										<p>
+											Donec maximus erat quis magna tincidunt, et ullamcorper ex condimentum. Pellentesque 
+											volutpat lectus felis, sit amet dapibus tortor convallis sit amet. Quisque egestas sem quis 
+											augue porta, et iaculis massa consequat.
+										</p>
+										<a class="btn btn-style btn-style-outline mt-sm-5 mt-4" href="services.html"> Our Services</a>
 									</div>
 								</div>
 							</div>
-						<?php } mysqli_close($con); ?>
+						</div>
+					</li>
+				</div>
+				<div class="item">
+					<li>
+						<div class="slider-info banner-view banner-top1 bg bg2">
+							<div class="banner-info">
+								<div class="container">
+									<div class="banner-info-bg">
+										<h5>Helping you to stay Happy one</h5>
+										<p>
+											Donec maximus erat quis magna tincidunt, et ullamcorper ex condimentum. Pellentesque 
+											volutpat lectus felis, sit amet dapibus tortor convallis sit amet. Quisque egestas sem quis 
+											augue porta, et iaculis massa consequat.
+										</p>
+										<a class="btn btn-style btn-style-outline mt-sm-5 mt-4" href="services.html"> Our Services</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</div>
+				<div class="item">
+					<li>
+						<div class="slider-info banner-view banner-top2 bg bg2">
+							<div class="banner-info">
+								<div class="container">
+									<div class="banner-info-bg">
+										<h5>1 Lakh+ Patients trusted in our Hospital.</h5>
+										<p>
+											Donec maximus erat quis magna tincidunt, et ullamcorper ex condimentum. Pellentesque 
+											volutpat lectus felis, sit amet dapibus tortor convallis sit amet. Quisque egestas sem quis 
+											augue porta, et iaculis massa consequat.
+										</p>
+										<a class="btn btn-style btn-style-outline mt-sm-5 mt-4" href="services.html"> Our Services</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</div>
+				<div class="item">
+					<li>
+						<div class="slider-info banner-view banner-top3 bg bg2" >
+							<div class="banner-info">
+								<div class="container">
+									<div class="banner-info-bg">
+										<h5>The Largest and most respected Agencies</h5>
+										<p>
+											Donec maximus erat quis magna tincidunt, et ullamcorper ex condimentum. Pellentesque 
+											volutpat lectus felis, sit amet dapibus tortor convallis sit amet. Quisque egestas sem quis 
+											augue porta, et iaculis massa consequat.
+										</p>
+										<a class="btn btn-style btn-style-outline mt-sm-5 mt-4" href="services.html"> Our Services</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="w3l-features py-5">
+		<div class="container py-lg-5 py-3">
+			<div class="row main-cont-wthree-2">
+				<div class="col-lg-6 feature-grid-right">
+					<img src="<?php echo base_url('assets/images/healthcare.jpg');?>" class="img-fluid" alt="healthcare">
+				</div>
+				<div class="col-lg-6 feature-grid-left mt-lg-0 mt-sm-5 mt-4">
+					<h4 class="title-left">We are Providing Excellent Health Care</h4>
+					<p class="text-para">
+						Curabitur id gravida risus. Fusce eget ex fermentum, ultricies nisi ac sed, lacinia est.
+						Quisque ut lectus consequat, venenatis eros et, commodo risus. Nullam sit amet laoreet elit. 
+					</p>
+					<div class="stats_main text-center">
+						<div class="w3l-stats">
+							<div class="">
+								<img src="<?php echo base_url('assets/images/patients.png');?>" class="img-fluid">
+							</div>
+							<div class="info-feature mt-3">
+								<h5 class="w3l-stats-txt"><a href="#url">1,754</a></h5>
+								<p class="stats-text">Patients</p>
+							</div>
+						</div>
+						<div class="w3l-stats">
+							<div class="">
+								<img src="<?php echo base_url('assets/images/services.png');?>" class="img-fluid">
+							</div>
+							<div class="info-feature mt-3">
+								<h5 class="w3l-stats-txt"><a href="#url">457</a></h5>
+								<p class="stats-text">Services</p>
+							</div>
+						</div>
+						<div class="w3l-stats">
+							<div class="">
+								<img src="<?php echo base_url('assets/images/award.png');?>" class="img-fluid">
+							</div>
+							<div class="info-feature mt-3">
+								<h5 class="w3l-stats-txt"><a href="#url">1,395</a></h5>
+								<p class="stats-text">Awards</p>
+							</div>
+						</div>
 					</div>
 				</div>
-			</section>
+			</div>
 		</div>
-	</div>
-	<?php require('include/thirdpart.php');?>
+	</section>
+	<section class="w3l-video-sec">
+		<div class="video-inner py-5">
+			<div class="overlay1 py-lg-5 py-sm-3">
+				<div class="container">
+					<div class="video-content">
+						<img src="<?php echo base_url('assets/images/heart-big.png');?>" class="img-fluid" alt="" />
+						<h4><a href="#url">We Provide High Quality Services.</a></h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed obcaecati natus illum, placeat nam
+						consequatur! Proin a diam et dui fermentum dolor.</p>
+						<a href="#notify" class="play-button btn"><span class="fa fa-play pl-1" aria-hidden="true">
+						</span></a>
+						<div id="notify" class="notify-pop-overlay">
+							<div class="notify-popup">
+								<h5>Excellent Health Care Systems</h5>
+								<iframe src="https://player.vimeo.com/video/180825357" frameborder="0"
+								allow="autoplay; fullscreen" allowfullscreen></iframe>
+								<a class="close" href="#close">&times;</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="w3l-call-to-action_9 py-5">
+		<div class="call-w3">
+			<div class="container">
+				<div class=" main-cont-wthree-2">
+					<div class="left-contect-calls text-center">
+						<h3 class="title mb-sm-5 mb-4">Certificates & Standards</h3>
+						<div class="call-grids-w3 ">
+							<a href="#url" class="">
+								<img src="<?php echo base_url('assets/images/logo1.png');?>" class="img-fluid" alt="">
+							</a>
+							<a href="#url" class="">
+								<img src="<?php echo base_url('assets/images/logo2.png');?>" class="img-fluid" alt="">
+							</a>
+							<a href="#url" class="">
+								<img src="<?php echo base_url('assets/images/logo3.png');?>" class="img-fluid" alt="">
+							</a>
+							<a href="#url" class="">
+								<img src="<?php echo base_url('assets/images/logo4.png');?>" class="img-fluid" alt="">
+							</a>
+							<a href="#url" class="">
+								<img src="<?php echo base_url('assets/images/logo5.png');?>" class="img-fluid" alt="">
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php require ('include/footer.php'); ?>
+
 </body>
 </html>
